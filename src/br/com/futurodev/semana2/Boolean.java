bad61f08c9c8889f63cdbdcf8e874a3730268f44
albertofelipebarros <albertofelipebarros@gmail.com>
2022-07-13 20:24:43 -0300

    commit

---
+++ src/br/com/futurodev/semana2/Boolean.java
@@ -0,0 +1,43 @@
+package br.com.futurodev.semana2;
+public class Boolean {
+    public static void main(String[] args) {
+        boolean resultado = true && true; // true
+        if(resultado){
+            System.out.println("Executa AND");
+        }
+
+        resultado = true && false; // false
+        if(resultado)
+            System.out.println("Não Executa");
+
+
+        if(false && false)
+            System.out.println("Não Executa");
+
+        resultado = true || true;
+        if (resultado){
+            System.out.println("Executa OR");
+        }
+
+        resultado = true || false;
+        if (resultado){
+            System.out.println("Executa OR");
+        }
+
+        resultado = false || false;
+        if (resultado){
+            System.out.println("Não Executa OR");
+        }
+
+        int x = 10;
+        if (10 <= x || x < -10){ //ou maior ou igual a 10, ou menor do que -10
+            System.out.println("ou maior ou igual a 10, ou menor do que -10 ");
+        }
+//   or     x                           x
+//        ---------(-10) -------- (10) ---------
+//   and                     x
+        if (10 >= x && x > -10){ //intervalo entre -9 e 10
+            System.out.println("intervalo entre -9 e 10");
+        }
+    }
+}
--- src/br/com/futurodev/semana2/Exercicio2.java
+++ src/br/com/futurodev/semana2/Exercicio2.java
@@ -1,10 +1,7 @@
 package br.com.futurodev.semana2;
-
 import java.util.Scanner;
-
 public class Exercicio2 {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
         System.out.print("Digite o primeiro número: ");
         int numero1 = scanner.nextInt();
@@ -28,7 +25,6 @@ public class Exercicio2 {
                 else if (resultado > 0) {
                 System.out.println("O resultado é " + resultado + " ele é IMPAR e é um número Positivo");
             }
-
         } else if (operacao == 2) {
             int resultado = numero1 - numero2;
             if (resultado % 2 == 0){
@@ -38,7 +34,6 @@ public class Exercicio2 {
                 else if (resultado > 0) {
                     System.out.println("O resultado é " + resultado + " ele é PAR e é um número Positivo");
                 }
-
             }
             else if (resultado < 0) {
                 System.out.println("O resultado é " + resultado + " ele é IMPAR e é um número Negativo");
@@ -56,7 +51,6 @@ public class Exercicio2 {
                 else if (resultado > 0) {
                     System.out.println("O resultado é " + resultado + " ele é PAR e é um número Positivo");
                 }
-
             }
             else if (resultado < 0) {
                 System.out.println("O resultado é " + resultado + " ele é IMPAR e é um número Negativo");
@@ -74,7 +68,6 @@ public class Exercicio2 {
                 else if (resultado > 0) {
                     System.out.println("O resultado é " + resultado + " ele é PAR e é um número Positivo");
                 }
-
             }
             else if (resultado < 0) {
                 System.out.println("O resultado é " + resultado + " ele é IMPAR e é um número Negativo");
