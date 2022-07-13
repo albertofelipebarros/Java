5f2ef2806a282a74a607b0faf2acabce2279db26
albertofelipebarros <albertofelipebarros@gmail.com>
2022-07-13 16:39:35 -0300

    commit

---
+++ src/br/com/futurodev/semana2/Exercicio6.java
@@ -0,0 +1,22 @@
+package br.com.futurodev.semana2;
+
+import java.util.Scanner;
+
+public class Exercicio6 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite o primeiro valor do Triângulo: ");
+        int x = scanner.nextInt();
+        System.out.print("Digite o segundo valor do Triângulo: ");
+        int y = scanner.nextInt();
+        System.out.print("Digite o terceiro valor do Triângulo: ");
+        int z = scanner.nextInt();
+        if (x == y && x == z){
+            System.out.println("Três lados iguais: Triângulo Equilátero");
+        }else if(x == y || x == z){
+            System.out.println("Dois lados iguais: Triângulo Isósceles");
+        }else
+            System.out.println("Três lados diferentes: Triângulo Escaleno");
+    }
+}
