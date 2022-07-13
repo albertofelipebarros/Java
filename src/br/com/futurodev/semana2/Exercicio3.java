35b94d272e63af9094b877f0c9fdf0efca711c7e
albertofelipebarros <albertofelipebarros@gmail.com>
2022-07-13 14:35:51 -0300

    commit

--- src/br/com/futurodev/semana2/Exercicio3.java
+++ src/br/com/futurodev/semana2/Exercicio3.java
@@ -10,15 +10,16 @@ public class Exercicio3 {
             int numero1 = scanner.nextInt();
             System.out.print("Digite o segundo número: ");
             int numero2 = scanner.nextInt();
+            if(numero1==numero2){
+                System.out.println("Numeros iguais");
+            }
             if(numero1>numero2){
-                System.out.println(numero1 + " é maior que " + numero2);
+                System.out.println("Maior: " + numero1);
+                System.out.print("Menor: " + numero2);
             }
-            else
-            if (numero1==numero2) {
-                System.out.println(numero1 + " é igual a " + numero2);
+            if (numero1<numero2) {
+                System.out.println("Maior: " + numero2);
+                System.out.println("Menor: " + numero1);
             }
-                else
-                System.out.println(numero1 + " é menor que " + numero2);
-
         }
     }
