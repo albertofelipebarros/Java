69d240b0b93454ce4cb7871336ee28cd37fb773c
albertofelipebarros <albertofelipebarros@gmail.com>
2022-07-13 16:15:46 -0300

    commit

--- src/br/com/futurodev/semana2/Exercicio2.java
+++ src/br/com/futurodev/semana2/Exercicio2.java
@@ -13,20 +13,75 @@ public class Exercicio2 {
         System.out.print("(1)Soma \n(2)Subtração \n(3)Multiplicação \n(4)Divisão \nDigite o numero da operação que deseja realizar: ");
         int operacao = scanner.nextInt();
         if (operacao == 1) {
-            int adicao = numero1 + numero2;
-            System.out.println("A soma é " + adicao);
-        } else if (operacao == 2) {
-            int subtracao = numero1 - numero2;
-            System.out.println("O resultado da subtração é " + subtracao);
+            int resultado = numero1 + numero2;
+            if (resultado % 2 == 0) {
+                         if (resultado < 0) {
+                             System.out.println("O resultado é " + resultado + " ele é PAR e é um número Negativo");
+                         }
+                         else if (resultado > 0) {
+                         System.out.println("O resultado é " + resultado + " ele é PAR e é um número Positivo");
+                         }
+                      }
+                else if (resultado < 0) {
+                System.out.println("O resultado é " + resultado + " ele é IMPAR e é um número Negativo");
+                 }
+                else if (resultado > 0) {
+                System.out.println("O resultado é " + resultado + " ele é IMPAR e é um número Positivo");
+            }
 
+        } else if (operacao == 2) {
+            int resultado = numero1 - numero2;
+            if (resultado % 2 == 0){
+                if (resultado < 0) {
+                    System.out.println("O resultado é " + resultado + " ele é PAR e é um número Negativo");
+                }
+                else if (resultado > 0) {
+                    System.out.println("O resultado é " + resultado + " ele é PAR e é um número Positivo");
+                }
+
+            }
+            else if (resultado < 0) {
+                System.out.println("O resultado é " + resultado + " ele é IMPAR e é um número Negativo");
+            }
+            else if (resultado > 0) {
+                System.out.println("O resultado é " + resultado + " ele é IMPAR e é um número Positivo");
+            }
         }
         else if (operacao == 3) {
-            int mult = numero1 * numero2;
-            System.out.println("O resultado da Multiplicação é " + mult);
+            int resultado = numero1 * numero2;
+            if (resultado % 2 == 0){
+                if (resultado < 0) {
+                    System.out.println("O resultado é " + resultado + " ele é PAR e é um número Negativo");
+                }
+                else if (resultado > 0) {
+                    System.out.println("O resultado é " + resultado + " ele é PAR e é um número Positivo");
+                }
+
+            }
+            else if (resultado < 0) {
+                System.out.println("O resultado é " + resultado + " ele é IMPAR e é um número Negativo");
+            }
+            else if (resultado > 0) {
+                System.out.println("O resultado é " + resultado + " ele é IMPAR e é um número Positivo");
+            }
         }
         else if (operacao == 4) {
-            int div = numero1 / numero2;
-            System.out.println("O resultado da Divisão é " + div);
+            int resultado = numero1 / numero2;
+            if (resultado % 2 == 0){
+                if (resultado < 0) {
+                    System.out.println("O resultado é " + resultado + " ele é PAR e é um número Negativo");
+                }
+                else if (resultado > 0) {
+                    System.out.println("O resultado é " + resultado + " ele é PAR e é um número Positivo");
+                }
+
+            }
+            else if (resultado < 0) {
+                System.out.println("O resultado é " + resultado + " ele é IMPAR e é um número Negativo");
+            }
+            else if (resultado > 0) {
+                System.out.println("O resultado é " + resultado + " ele é IMPAR e é um número Positivo");
+            }
         }
         else
             System.out.println("A operação digitada é inválida");
