6ff41d7befb4ea98c2c3d034903b028b52f240ba
albertofelipebarros <albertofelipebarros@gmail.com>
2022-07-14 20:59:46 -0300

    commit

--- src/br/com/futurodev/semana2/switchapp.java
+++ src/br/com/futurodev/semana2/switchapp.java
@@ -1,7 +1,40 @@
 package br.com.futurodev.semana2;
 
+import javax.sound.midi.Soundbank;
+import java.util.Scanner;
+
 public class switchapp {
     public static void main(String[] args) {
-        System.out.println("");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite 1 numero");
+        int numero = scanner.nextInt();
+        switch (numero) {
+            case 0:
+                System.out.println("0");
+                break;
+            case 1:
+                System.out.println("1");
+            case 2:
+                System.out.println("2");
+                break;
+            case 3:
+            default:
+                System.out.println("defaut");
+        }
+        System.out.println("Apos o Switch");
+        System.out.println();
+        Scanner scanner2 = new Scanner(System.in);
+        System.out.println("Digite 1 nome");
+        String nome = scanner2.nextLine();
+        switch (nome) {
+            case "default" :
+                System.out.println("negado");
+                break;
+            case "andre":
+            case "alberto":
+            case "luana":
+                System.out.println("Aceito");
+                break;
+        }
     }
 }
