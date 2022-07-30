3550a6a4bfa0c50e9d5552612e042fe092d61513
Alberto Felipe <109188168+albertofelipebarros@users.noreply.github.com>
2022-07-30 13:42:00 -0300

    commit;

--- src/br/com/futurodev/semana4/Exercicio6.java
+++ src/br/com/futurodev/semana4/Exercicio6.java
@@ -1,17 +1,25 @@
 package br.com.futurodev.semana4;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.TreeSet;
 
 public class Exercicio6 {
     public static void main(String[] args) {
-        HashSet<Integer> hashSet = new HashSet<>();
-        hashSet.add(1);
-        hashSet.add(2);
-        hashSet.add(3);
-        hashSet.add(3);
-        hashSet.add(4);
-        System.out.println("HashSet: " + hashSet);
-        System.out.println("Removendo numero repetido " + hashSet.remove(3));
-        System.out.println("HashSet: " + hashSet);
+        ArrayList<Integer> listanumeros = new ArrayList();
+        listanumeros.add(10);
+        listanumeros.add(11);
+        listanumeros.add(12);
+        listanumeros.add(10);
+        listanumeros.add(11);
+        listanumeros.add(10);
+
+        TreeSet<Integer> setNumeros = new TreeSet<>();
+        setNumeros.addAll(listanumeros);
+        int setSizes = setNumeros.size();
+        for (int i = 0; i < setSizes; i++) {
+            System.out.println(setNumeros.pollLast());
+            
+        }
     }
 }
